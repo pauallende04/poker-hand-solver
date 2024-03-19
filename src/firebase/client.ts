9f1aaf5a0c105e121f1db36114a9cd244e3508c5
pauallende <pauallendeherraiz@gmail.com>
2024-03-19 23:48:16 +0100

    last apply

---
+++ src/firebase/client.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyACm0NpegbXTxDYUGdxjvYTFTT-2SRINvI",
+  authDomain: "poker-hand-solver.firebaseapp.com",
+  projectId: "poker-hand-solver",
+  storageBucket: "poker-hand-solver.appspot.com",
+  messagingSenderId: "227792206317",
+  appId: "1:227792206317:web:08a70325ed363bb7620fe4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/pages/api/auth/register.ts
@@ -0,0 +1,34 @@
+import type { APIRoute } from "astro";
+import { getAuth } from "firebase-admin/auth";
+import { app } from "../../../firebase/server";
+
+export const POST: APIRoute = async ({ request, redirect }) => {
+  const auth = getAuth(app);
+
+  /* Obtener los datos del formulario */
+  const formData = await request.formData();
+  const email = formData.get("email")?.toString();
+  const password = formData.get("password")?.toString();
+  /* const name = formData.get("name")?.toString(); */
+
+  if (!email || !password) {
+    return new Response(
+      "Faltan datos del formulario",
+      { status: 400 }
+    );
+  }
+
+  /* Crear un usuario */
+  try {
+    await auth.createUser({
+      email,
+      password
+    });
+  } catch (error: any) {
+    return new Response(
+      "Algo salió mal",
+      { status: 400 }
+    );
+  }
+  return redirect("/signin");
+};
